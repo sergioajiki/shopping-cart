0f99aa3bf21dea7acc28c6c1e876e2d285560f3a
Sergio <sergioajk@hotmail.com>
2023-01-17 17:21:23 -0300

    requisito 6

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,29 +2,20 @@ export const fetchProduct = async (item) => {
   if (!item) {
     throw new Error('ID não informado');
   }
-  try {
-    const apiMBproduto = `https://api.mercadolibre.com/items/${item}`;
-    const response = await (fetch(apiMBproduto));
-    const data = await response.json();
-    // console.log(data)
-    return data;
-  } catch (error) {
-    return error.message;
-  }
+  const apiMBproduto = `https://api.mercadolibre.com/items/${item}`;
+  const response = await (fetch(apiMBproduto));
+  const data = await response.json();
+  // console.log(data)
+  return data;
 };
 
 export const fetchProductsList = async (grupo) => {
   if (!grupo) {
     throw new Error('Termo de busca não informado');
   }
-
-  try {
-    const apiMB = `https://api.mercadolibre.com/sites/MLB/search?q=${grupo}`;
-    const response = await (fetch(apiMB));
-    const data = await response.json();
-    // console.log(data)
-    return data.results;
-  } catch (error) {
-    return error.message;
-  }
+  const apiMB = `https://api.mercadolibre.com/sites/MLB/search?q=${grupo}`;
+  const response = await (fetch(apiMB));
+  const data = await response.json();
+  // console.log(data)
+  return data.results;
 };
