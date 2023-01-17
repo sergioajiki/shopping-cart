be016faf92162be1f13a22f5c3038c8e3b295722
Sergio <sergioajk@hotmail.com>
2023-01-16 22:03:59 -0300

    requisito 4

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 // fetchProduct colocar depois
 import './style.css';
 
+function showLoading() {
+  const Newdiv = document.createElement('div');
+  const labelCarregando = document.createElement('label');
+  Newdiv.classList.add('loading');
+  Newdiv.appendChild(labelCarregando);
+  labelCarregando.innerText = 'Carregando...';
+  document.querySelector('.container').appendChild(Newdiv);
+}
+function hideLoading() {
+  document.querySelector('.loading').remove();
+}
+
+window.onload = showLoading;
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const conteinerProdutos = document.querySelector('.products');
@@ -12,7 +25,12 @@ const conteinerProdutos = document.querySelector('.products');
 const produtosRecebidosdaAPI = await fetchProductsList('computador');
 // console.log(produtosRecebidosdaAPI);
 
-produtosRecebidosdaAPI.forEach((item) => {
-  const exibirProdutos = createProductElement(item);
-  conteinerProdutos.appendChild(exibirProdutos);
-});
+const buildProductsList = () => {
+  hideLoading();
+  produtosRecebidosdaAPI.forEach((item) => {
+    const exibirProdutos = createProductElement(item);
+    conteinerProdutos.appendChild(exibirProdutos);
+  });
+};
+
+buildProductsList();
